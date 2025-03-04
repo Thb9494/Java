4e050c8ad07d089a0af63ce17807a9d6856295a7
THB9494 <thb9494@thi.de>
2025-03-04 09:53:47 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+        System.out.println("Hello World");
+    }
+}
